b88d6607dacde130fda641057ff9cccca40fa73a
Alexander Papirnyk <gennerys1515@gmail.com>
2019-04-07 21:55:28 +0300

    arithmetic coding added

---
+++ ArithmeticCoding/MyTest/MyArithTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyTest
+{
+    [TestClass]
+    public class MyArithTests
+    {
+        [TestMethod]
+        public void TestOutputPrinterWithCapsLockString()
+        {
+            //arrange
+            string word = "PROGRAM";
+            //act
+            ArithmeticCoding.Output.Printer(word);
+            //assert
+            Assert
+        }
+    }
+}
---
+++ ArithmeticCoding/MyTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("091f0a2a-9d3c-498f-a860-c9410943feeb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
