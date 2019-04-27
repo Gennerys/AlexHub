2cb1cb6d24bc81fdf06531b691e4e2dae959ec7d
Alexander Papirnyk <gennerys1515@gmail.com>
2019-04-27 21:56:16 +0300

    ArithCoding added

---
+++ ArithmeticCoding/ArithmeticCoding/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Output.Printer("PROGRAM");
+            Output.Printer("testtttt");
+            Output.Printer("Papirnyk");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
